5e6640db4826ae273a118dddc21754482e05cefd
gunjiamulya465 <gunjig465@gmail.com>
2026-08-16 16:47:35 +0530

    Create heart_disease_prediction.py

--- heart_disease_prediction.py
+++ heart_disease_prediction.py
@@ -1,1 +1,18 @@
 
+import pandas as pd
+
+# Load Heart Disease dataset
+df = pd.read_csv("Heart.csv")
+
+# Display first 5 rows
+print(df.head())
+
+# Display dataset information
+print("\nDataset Shape:")
+print(df.shape)
+
+print("\nColumn Names:")
+print(df.columns)
+
+print("\nMissing Values:")
+print(df.isnull().sum())
